a2fbe1d5fd1c425f80c919471d177b5dc44cedb3
naustin36 <naustin3.6@gmail.com>
2026-09-06 22:47:25 -0600

    redesigned sort_file() to allow any number of categories, subfolders, and filetypes to sort

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### File Sorter CLI Tool
 
-When run, moves all supported filetypes from the source folder to the destination folder.
+When run, moves all selected filetypes from the source folder to the destination folder.
 
 The source and destination folders, the specific folders files are sorted into, and which filetypes are sorted can all be configured in config.json.
+
+The config file is created automatically on first run. Source and destination folders must then be set in config.json before the tool can be used, as no default folders are provided.
--- file_management.py
+++ file_management.py
@@ -3,31 +3,19 @@ from config import *
 
 config = load_config()
 destination = Path(config["destination_folder"]).expanduser()
+file_categories = config["media_types"]
 
-def sort_file(file: Path):
-    if file.suffix in config["photo"]:
-        print(f"Image Found: {file.name}")
-        final_destination = destination.joinpath("photo")
-        print(f"Destination folder: {final_destination}")
-        move_file(file, final_destination)
-    if file.suffix in config["video"]:
-        print(f"Video Found: {file.name}")
-        final_destination = destination.joinpath("video")
-        print(f"Destination folder: {final_destination}")
-        move_file(file, final_destination)
-    if file.suffix in config["gif"]:
-        print(f".gif found: {file.name}")
-        final_destination = destination.joinpath("gif")
-        print(f"Destination folder: {final_destination}")
-        move_file(file, final_destination)
+def sort_file(file: Path) -> None:
+    for category in file_categories:
+        if file.suffix in file_categories[category]:
+            final_destination = destination.joinpath(category)
+            move_file(file, final_destination)
 
-def move_file(file: Path, destination_path: Path):
-    print("Moving file:",file.name)
-    print("Moving to:",destination_path)
+def move_file(file: Path, destination_path: Path) -> None:
+    print(f"Moving {file.name} to '{destination_path}'")
     if not destination_path.exists():
         destination_path.mkdir()
     final_destination = destination_path.joinpath(file.name)
-    print("TEST/",final_destination)
     file.rename(final_destination)
 
 def load_source_folder(source_path: Path) -> list[Path]:
